a47529ce63318f5a090bef43ebe85127faf5775b
Wangerion <None>
2022-03-08 01:22:36 +0200

    Different UI changes

--- helpers.py
+++ helpers.py
@@ -23,19 +23,30 @@ def cityfinder(cityname):
     try:
         api1 = finder('apikey.json','API_Key' )
         apikey = api1.load()
-        url = f"http://api.openweathermap.org/data/2.5/weather?q={urllib.parse.quote_plus(cityname)}&appid={apikey}&units=metric"
+        streetmapurl = f"   https://nominatim.openstreetmap.org/search?city={urllib.parse.quote_plus(cityname)}&format=json&addressdetails=1&limit=1"
+        r = requests.get(streetmapurl)
+        r.raise_for_status()
+        coord = r.json()
+        lat = coord[0]['lat']
+        lon = coord[0]['lon']
+        #url = f"http://api.openweathermap.org/data/2.5/weather?q={urllib.parse.quote_plus(cityname)}&appid={apikey}&units=metric"
+        url = f"https://api.openweathermap.org/data/2.5/onecall?lat={urllib.parse.quote_plus(lat)}&lon={urllib.parse.quote_plus(lon)}&appid={apikey}&units=metric"
         response = requests.get(url)
         response.raise_for_status()
     except requests.RequestException:
         return None
     try:
         data = response.json()
+
+        with open("weathermap.json", "w") as write_file:
+            json.dump(data, write_file, indent=4)
+
         return {
-            "temp": round(data['main']['temp']),
-            "feeltemp": round(data['main']['feels_like']),
-            "wind": round(data['wind']['speed']),
-            #"icons": "https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0027_light_snow_showers_night.png", #data['current']['weather_icons'],
-            "description": data['weather'][0]['description']
+            "temp": round(data['current']['temp']),
+            "feeltemp": round(data['current']['feels_like']),
+            "wind": round(data['current']['wind_speed']),
+            "icons": data['current']['weather'][0]['icon'],
+            "description": data['current']['weather'][0]['description']
         }
     except (KeyError, TypeError, ValueError):
         return None
