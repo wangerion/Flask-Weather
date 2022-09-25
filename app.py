aa7aa64eacb6ed0b25e7f30a62ec4348b87e38c6
wangerion <wavesvibe@gmail.com>
2022-09-25 23:27:28 +0300

    Test

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, flash, render_template, request
+import os
 import requests
 from werkzeug.utils import redirect
 from helpers import cityfinder
@@ -6,6 +7,14 @@ from helpers import cityfinder
 
 app = Flask(__name__)
 
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+app.secret_key = SECRET_KEY
+
+units = {
+    '˚C, m/s': 'metric',
+    '˚F, mph': 'imperial'
+}
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -13,7 +22,12 @@ def home():
 
     if request.method == 'POST':
         city = request.form.get("city")
+        temp = request.form.get("tempvalues")
+        print(temp)
         print(cityfinder(city))
-        return render_template("index.html", city=cityfinder(city))
+        if not cityfinder(city):
+            flash("Unable to search for that city. Try again!", category='error')
+            return render_template("index.html", units=units)
+        return render_template("index.html", city=cityfinder(city), units=units)
     else:
-        return render_template("index.html")
+        return render_template("index.html", units=units)
--- helpers.py
+++ helpers.py
@@ -23,16 +23,19 @@ def cityfinder(cityname):
     try:
         api1 = finder('apikey.json','API_Key' )
         apikey = api1.load()
-        streetmapurl = f"   https://nominatim.openstreetmap.org/search?city={urllib.parse.quote_plus(cityname)}&format=json&addressdetails=1&limit=1"
+        streetmapurl = f"https://nominatim.openstreetmap.org/search?city={urllib.parse.quote_plus(cityname)}&format=json&addressdetails=1&limit=1"
         r = requests.get(streetmapurl)
         r.raise_for_status()
         coord = r.json()
-        lat = coord[0]['lat']
-        lon = coord[0]['lon']
-        #url = f"http://api.openweathermap.org/data/2.5/weather?q={urllib.parse.quote_plus(cityname)}&appid={apikey}&units=metric"
-        url = f"https://api.openweathermap.org/data/2.5/onecall?lat={urllib.parse.quote_plus(lat)}&lon={urllib.parse.quote_plus(lon)}&appid={apikey}&units=metric"
-        response = requests.get(url)
-        response.raise_for_status()
+        if coord:
+            lat = coord[0]['lat']
+            lon = coord[0]['lon']
+            #url = f"http://api.openweathermap.org/data/2.5/weather?q={urllib.parse.quote_plus(cityname)}&appid={apikey}&units=metric"
+            url = f"https://api.openweathermap.org/data/2.5/onecall?lat={urllib.parse.quote_plus(lat)}&lon={urllib.parse.quote_plus(lon)}&appid={apikey}&units=metric"
+            response = requests.get(url)
+            response.raise_for_status()
+        else:
+            return None
     except requests.RequestException:
         return None
     try:
@@ -46,7 +49,12 @@ def cityfinder(cityname):
             "feeltemp": round(data['current']['feels_like']),
             "wind": round(data['current']['wind_speed']),
             "icons": data['current']['weather'][0]['icon'],
-            "description": data['current']['weather'][0]['description']
+            "description": data['current']['weather'][0]['description'],
+            "humidity": data['current']['humidity'],
+            "dewpoint": round(data['current']['dew_point']),
+            "uvi": data['current']['uvi'],
+            "pressure": data['current']['pressure'],
+            "vis": data['current']['visibility']
         }
     except (KeyError, TypeError, ValueError):
         return None
